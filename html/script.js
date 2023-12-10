8ef51618c270dcd90bf56cad4350b2d261ac1775
DevLung <123473305+DevLung@users.noreply.github.com>
2023-12-10 20:05:00 +0100

    make rules popup close when clicking outside of popup

--- html/script.js
+++ html/script.js
@@ -2,9 +2,13 @@ $ = function (id) {
     return document.getElementById(id);
 }
 
+let openedOverlay;
+
 var show = function (id) {
     $(id).style.display = 'block';
+    openedOverlay = id;
 }
 var hide = function (id) {
     $(id).style.display = 'none';
+    openedOverlay = null;
 }
