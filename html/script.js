a568a053b341f61522546648819a23a429a65ec6
DevLung <123473305+DevLung@users.noreply.github.com>
2024-05-19 16:01:22 +0200

    clean up script.js

--- html/script.js
+++ html/script.js
@@ -1,20 +1,16 @@
 // import "jquery"; // uncomment while developing for intellisense
 
 
-$ = function (id) {
-    return document.getElementById(id);
-}
-
 var openedOverlay;
 
 // open popup and save its ID in openedOverlay
-var show = function (id) {
-    $(id).style.display = 'block';
+function show(id) {
+    document.getElementById(id).style.display = 'block';
     openedOverlay = id;
 }
 // close popup
-var hide = function (id) {
-    $(id).style.display = 'none';
+function hide(id) {
+    document.getElementById(id).style.display = 'none';
     openedOverlay = null;
 }
 
